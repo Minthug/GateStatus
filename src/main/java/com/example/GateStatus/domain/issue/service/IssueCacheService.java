dbf3c9bf8d2e6efad3fc11216989e1e343eb42c6
minthug <jkisimmortal4@gmail.com>
2024-12-10 16:23:36 +0900

    Add methods for caching, retrieving, and cleaning issues

--- src/main/java/com/example/GateStatus/domain/issue/service/IssueCacheService.java
+++ src/main/java/com/example/GateStatus/domain/issue/service/IssueCacheService.java
@@ -1,15 +1,26 @@
 package com.example.GateStatus.domain.issue.service;
 
+import com.example.GateStatus.domain.issue.Issue;
 import com.example.GateStatus.domain.issue.repository.IssueRepository;
 import com.example.GateStatus.domain.issue.repository.response.IssueRedisDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -52,5 +63,41 @@ public class IssueCacheService {
                     cacheIssueInfo(issueRedisDto);
                     return issueRedisDto;
                 });
+    }
+
+    public List<IssueRedisDto> getRecentIssues(int limit) {
+        Set<Object> issues = redisTemplate.opsForZSet()
+                .reverseRange(RECENT_ISSUES_KEY, 0, limit - 1);
+
+        if (issues != null || !issues.isEmpty()) {
+            return issues.stream()
+                    .filter(obj -> obj instanceof IssueRedisDto)
+                    .map(obj -> (IssueRedisDto) obj)
+                    .collect(Collectors.toList());
+        }
+
+        Pageable pageable = PageRequest.of(0, limit, Sort.by("createdAt").descending());
+        List<Issue> recentIssues = issueRepository.findAllByCreatedAtDesc(pageable);
+        List<IssueRedisDto> issueRedisDtos = recentIssues.stream()
+                .map(IssueRedisDto::from)
+                .collect(Collectors.toList());
+
+        issueRedisDtos.forEach(dto -> {
+            cacheIssueInfo(dto);
+            addToRecentIssues(dto);
+        });
+        return issueRedisDtos;
+    }
+
+    public void deleteIssueCache(Long issueId) {
+        redisTemplate.delete(ISSUE_INFO_PREFIX + issueId);
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void cleanOldIssues() {
+        LocalDateTime weekAgo = LocalDateTime.now().minusWeeks(1);
+        redisTemplate.opsForZSet().removeRangeByScore(RECENT_ISSUES_KEY,
+                0,
+                weekAgo.toEpochSecond(ZoneOffset.UTC));
     }
 }
