1c351d82da338134f29c89b5936c0cebb5f1b997
minthug <jkisimmortal4@gmail.com>
2024-12-12 16:53:07 +0900

    Add endpoints for updating, deleting, and filtering figures

--- src/main/java/com/example/GateStatus/domain/figure/controller/FigureController.java
+++ src/main/java/com/example/GateStatus/domain/figure/controller/FigureController.java
@@ -1,15 +1,20 @@
 package com.example.GateStatus.domain.figure.controller;
 
+import com.example.GateStatus.domain.figure.FigureType;
 import com.example.GateStatus.domain.figure.service.FigureService;
 import com.example.GateStatus.domain.figure.service.request.FindFigureCommand;
 import com.example.GateStatus.domain.figure.service.request.RegisterFigureCommand;
+import com.example.GateStatus.domain.figure.service.request.UpdateFigureCommand;
 import com.example.GateStatus.domain.figure.service.response.FindFigureDetailResponse;
 import com.example.GateStatus.domain.figure.service.response.RegisterFigureResponse;
+import com.example.GateStatus.domain.figure.service.response.UpdateFigureResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,4 +43,23 @@ public class FigureController {
         PageRequest pageRequest = PageRequest.of(page, size);
         return ResponseEntity.ok(figureService.findAllFigures(pageRequest, type, keyword));
     }
+
+    @GetMapping("/v1/type/{figureId}")
+    public ResponseEntity<List<FindFigureDetailResponse>> findFiguresByType(@PathVariable FigureType figureType) {
+        return ResponseEntity.ok(figureService.findFiguresByType(figureType));
+    }
+
+    @PatchMapping("/{figureId}")
+    public ResponseEntity<UpdateFigureResponse> updateFigure(@PathVariable Long figureId,
+                                                             @RequestBody UpdateFigureCommand figureCommand) {
+        UpdateFigureResponse response = figureService.updateFigure(figureId, figureCommand);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{figureId}")
+    public ResponseEntity<Void> deleteFigure(@PathVariable Long figureId) {
+        figureService.deleteFigure(figureId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
