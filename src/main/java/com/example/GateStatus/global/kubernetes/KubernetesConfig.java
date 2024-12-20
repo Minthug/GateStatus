417469e3bc5d8cdf2c88167eea9033552fe4bd70
minthug <jkisimmortal4@gmail.com>
2024-12-20 16:28:57 +0900

    Add Kubernetes configuration support through Spring integration

---
+++ src/main/java/com/example/GateStatus/global/kubernetes/KubernetesConfig.java
@@ -0,0 +1,9 @@
+package com.example.GateStatus.global.kubernetes;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+public class KubernetesConfig {
+}
