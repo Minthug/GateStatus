68a264595250cfc491830421166c39c6ac17858e
minthug <jkisimmortal4@gmail.com>
2025-04-07 18:12:09 +0900

    Add UpdateCategoryCommand and CategoryResponse models

---
+++ backend/src/main/java/com/example/GateStatus/domain/category/service/request/UpdateCategoryCommand.java
@@ -0,0 +1,7 @@
+package com.example.GateStatus.domain.category.service.request;
+
+import com.example.GateStatus.domain.category.CategoryType;
+
+public record UpdateCategoryCommand(String name, String description, String iconUrl,
+                                    Integer displayOrder, Boolean active, CategoryType type) {
+}
---
+++ backend/src/main/java/com/example/GateStatus/domain/category/service/response/CategoryResponse.java
@@ -0,0 +1,26 @@
+package com.example.GateStatus.domain.category.service.response;
+
+import com.example.GateStatus.domain.category.Category;
+import com.example.GateStatus.domain.category.CategoryType;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+public record CategoryResponse(Long id, String name, String description, String iconUrl,
+                               int displayOrder, boolean active, CategoryType type,
+                               LocalDateTime createdDate, LocalDateTime modifiedDate) {
+
+    public static CategoryResponse from(Category category) {
+        return new CategoryResponse(
+                category.getId(),
+                category.getName(),
+                category.getDescription(),
+                category.getIconUrl(),
+                category.getDisplayOrder(),
+                category.isActive(),
+                category.getType(),
+                category.getCreatedDate(),
+                category.getModifiedDate()
+        );
+    }
+}
