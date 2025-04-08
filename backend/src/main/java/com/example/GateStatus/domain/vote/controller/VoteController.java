341c6a552f8918573bd2057f55000352dce11803
minthug <jkisimmortal4@gmail.com>
2025-04-08 18:36:01 +0900

    Add VoteController to handle vote-related endpoints

---
+++ backend/src/main/java/com/example/GateStatus/domain/vote/controller/VoteController.java
@@ -0,0 +1,25 @@
+package com.example.GateStatus.domain.vote.controller;
+
+import com.example.GateStatus.domain.vote.service.BillVoteDTO;
+import com.example.GateStatus.domain.vote.service.VoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/votes")
+@RequiredArgsConstructor
+public class VoteController {
+
+    private final VoteService voteService;
+
+    @GetMapping("/figure/{figureId}")
+    public ResponseEntity<List<BillVoteDTO>> getVotesFigureId(@PathVariable Long figureId) {
+        return ResponseEntity.ok(voteService.getVotesByFigureId(figureId));
+    }
+}
