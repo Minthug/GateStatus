1d6001819d340180484a6e72d3b14897d188b5e8
minthug <jkisimmortal4@gmail.com>
2025-04-22 18:58:57 +0900

    Add RabbitMQ integration with configuration and publisher

--- backend/src/main/java/com/example/GateStatus/global/config/rabbitMQ/RabbitEventPublisher.java
+++ backend/src/main/java/com/example/GateStatus/global/config/rabbitMQ/RabbitEventPublisher.java
@@ -1,4 +1,15 @@
 package com.example.GateStatus.global.config.rabbitMQ;
 
-public class RabbitEventPublisher {
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitEventPublisher implements Event {
+
+
+    private final RabbitTemplate rabbitTemplate;
+
+
 }
---
+++ backend/src/main/java/com/example/GateStatus/global/config/rabbitMQ/RabbitMQConfig.java
@@ -0,0 +1,49 @@
+package com.example.GateStatus.global.config.rabbitMQ;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost(host);
+        factory.setPort(port);
+        factory.setUsername(username);
+        factory.setPassword(password);
+        return factory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setConcurrentConsumers(3);
+        factory.setMaxConcurrentConsumers(10);
+
+        return factory;
+    }
+}
