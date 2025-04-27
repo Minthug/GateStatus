a9299029f1fe38a71a6777c88c95b667b594886e
minthug <jkisimmortal4@gmail.com>
2025-04-27 16:37:03 +0900

    Add logging to CategoryAdminController and create test file

--- backend/src/main/java/com/example/GateStatus/domain/category/controller/CategoryAdminController.java
+++ backend/src/main/java/com/example/GateStatus/domain/category/controller/CategoryAdminController.java
@@ -6,6 +6,7 @@ import com.example.GateStatus.domain.category.service.request.CreateCategoryComm
 import com.example.GateStatus.domain.category.service.request.UpdateCategoryCommand;
 import com.example.GateStatus.domain.category.service.response.CategoryResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +16,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/admin/categories")
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryAdminController {
 
     private final CategoryService categoryService;
 
     @GetMapping
     public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+
+        log.info("GET 메서드 실행");
         return ResponseEntity.ok(categoryService.getAllActiveCategories());
     }
 
@@ -38,6 +42,7 @@ public class CategoryAdminController {
     @PatchMapping
     public ResponseEntity<CategoryResponse> updateCategory(@PathVariable Long id,
                                                            @RequestBody UpdateCategoryCommand command) {
+
         CategoryResponse response = categoryService.updateCategory(id, command);
 
         return ResponseEntity.ok(response);
