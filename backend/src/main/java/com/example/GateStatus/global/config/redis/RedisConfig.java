1c3e2d94a936686e2a5b6ff982f9e82d74d718aa
minthug <jkisimmortal4@gmail.com>
2025-05-15 17:41:57 +0900

    Add support for enhanced JSON serialization in Redis

--- backend/src/main/java/com/example/GateStatus/global/config/redis/RedisConfig.java
+++ backend/src/main/java/com/example/GateStatus/global/config/redis/RedisConfig.java
@@ -1,7 +1,10 @@
 package com.example.GateStatus.global.config.redis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +15,12 @@ import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Links;
+import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
+
+import java.io.IOException;
 
 @Configuration
 public class RedisConfig {
@@ -31,9 +38,24 @@ public class RedisConfig {
     @Bean
     public ObjectMapper redisObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
 
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+        objectMapper.registerModule(new Jackson2HalModule());
+
+        objectMapper.addMixIn(Links.class, LinkMixin.class);
+
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+
         return objectMapper;
     }
 
@@ -43,8 +65,8 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
 
-        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        serializer.setObjectMapper(redisObjectMapper());
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(redisObjectMapper());
 
         redisTemplate.setValueSerializer(serializer);
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
@@ -66,4 +88,27 @@ public class RedisConfig {
     public ListOperations<String, String> listOperations(RedisTemplate<String, String> redisTemplate) {
         return redisTemplate.opsForList();
     }
+
+    @JsonSerialize(using = LinkSerializer.class)
+    abstract static class LinksMixIn {
+
+    }
+
+    static class LinkSerializer extends StdSerializer<Links> {
+        private static final long serialVersionUID = 1L;
+
+        public LinkSerializer() {
+            super(Links.class);
+        }
+
+        @Override
+        public void serialize(Links value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+            gen.writeStartObject();
+            for (Link link : value) {
+                gen.writeStringField(link.getRel().value(), link.getHref());
+            }
+            gen.writeEndObject();
+        }
+    }
 }
+
